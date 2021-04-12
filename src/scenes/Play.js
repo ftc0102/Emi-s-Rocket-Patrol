206c093b3d92ca577c6a5417f39eafe9988a8b91
Emily Barnes <evmbarne@ucsc.edu>
2021-04-12 15:05:15 -0700

    explosions! >:)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -34,6 +34,13 @@ class Play extends Phaser.Scene {
        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
+       //animation configuration
+       this.anims.create({
+           key: "explode",
+           frames: this.anims.generateFrameNumbers("explosion", {start: 0, end:9, first:0}),
+           frameRate: 30
+       })
+
     }
 
     update(){
@@ -42,17 +49,18 @@ class Play extends Phaser.Scene {
         this.ship01.update();
         this.ship02.update();
         this.ship03.update();
+        //checking collision
         if (this.checkCollision(this.p1Rocket, this.ship03)){
             this.p1Rocket.reset();
-            this.ship03.reset();
+            this.shipExplode(this.ship03);
         }
         if (this.checkCollision(this.p1Rocket, this.ship02)){
             this.p1Rocket.reset();
-            this.ship02.reset();
+            this.shipExplode(this.ship02);
         }
         if (this.checkCollision(this.p1Rocket, this.ship01)){
             this.p1Rocket.reset();
-            this.ship01.reset();
+            this.shipExplode(this.ship01);
         }
     }
 
@@ -66,5 +74,19 @@ class Play extends Phaser.Scene {
         else{
             return false;
         }
+    }
+
+    shipExplode(ship){
+        //hide ship
+        ship.alpha = 0;
+        //create explosion
+        let boom = this.add.sprite(ship.x, ship.y, "explosion").setOrigin(0,0);
+        boom.anims.play("explode");
+        boom.on("animationcomplete", () => {
+            ship.reset();
+            ship.alpha = 1;
+            boom.destroy();
+        });
+
     }
 }
