461d9a3e1792fcfdf06999c44cffa39f5bfb3e54
Emily Barnes <evmbarne@ucsc.edu>
2021-04-19 17:06:09 -0700

    instructions updated

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -32,9 +32,10 @@
             //show menu text 
             this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, "ROCKET PATROL", menuConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2, "Use <--> arrows to move & (F) to fire", menuConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + borderUISize, "(G) shoots left and (H) shoots right", menuConfig).setOrigin(0.5);
             menuConfig.backgroundColor = "#00FF00";
             menuConfig.color = "#000";
-            this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, "Press <- for easy and -> for expert", menuConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + borderUISize*2+borderPadding, "Press <- for easy and -> for expert", menuConfig).setOrigin(0.5);
 
             //KEYS
             keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
