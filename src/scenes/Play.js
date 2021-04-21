15b7212b0b8b14d414bd24df8f9f91c76bb265c1
Emily Barnes <evmbarne@ucsc.edu>
2021-04-21 02:02:03 -0700

    fixed issue w/ clipping sprites

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -16,6 +16,7 @@ class Play extends Phaser.Scene {
     }
 
     create() { 
+        
         //animation config 
         this.anims.create({ //explosion
             key: "explode",
@@ -36,11 +37,6 @@ class Play extends Phaser.Scene {
        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2,0x00FF00).setOrigin(0,0);
        //add ufo 
        this.ufo1 = new Spaceship(this, game.config.width, borderUISize*9 + borderPadding, "ufo", 0, 100).setOrigin(0,0);
-       //White Border
-       this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
-       this.add.rectangle(0, game.config.height-borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
-       this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
-       this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
        //rocket
        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height-borderUISize-borderPadding, "rocket").setOrigin(0.5,0);
        //add 3 spaceships 
@@ -54,6 +50,11 @@ class Play extends Phaser.Scene {
        this.ship03.anims.play("greenSpaceship");
        //add energy ball
        this.ball = new Ball(this, game.config.width/2, game.config.height-borderUISize-borderPadding-12, "ball").setOrigin(0.5,0);
+       //White Border
+       this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
+       this.add.rectangle(0, game.config.height-borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
+       this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+       this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
        //controls
        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
