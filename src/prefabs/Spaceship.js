620f8adb1371760a989b6bb2ee69222a6e304afd
Emily Barnes <evmbarne@ucsc.edu>
2021-04-21 10:06:03 -0700

    spaceships move in different directions

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -7,19 +7,39 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.points = pointValue; //track points
         this.moveSpeed = game.settings.spaceshipSpeed; //pixels per frame
+        this.direction = Phaser.Math.Between(1,2);
     }
 
+
     update(){
-        //move spaceship left
-        this.x -= this.moveSpeed;
+        //move spaceship left or right
+        if (this.direction == 1){
+            this.x -= this.moveSpeed;
+        }
+
+        if (this.direction==2){
+            this.flipX = true;
+            this.x += this.moveSpeed;
+        }
         //wrap around
-        if(this.x <= 0 - this.width){
+        if(this.x <= 0 - this.width || this.x >= game.config.width){
             this.reset();   
         }
+
+
+
+
     }
 
     //position reset
     reset(){
-        this.x = game.config.width;
+        if(this.x >= game.config.width){
+            this.x = 0 - this.width + 1; 
+        } 
+
+        else{
+            this.x = game.config.width - 1; 
+        }
+
     }
 }
